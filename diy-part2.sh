eae3e8d8a10a2c0eed67fb364b152b5e604ba709
sunvid1 <86123106+sunvid1@users.noreply.github.com>
2021-06-21 11:01:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.5.1/g' package/base-files/files/bin/config_generate
-sed -i 's/default "5"/default "0"/g' config/Config-images.in
+#sed -i 's/default "5"/default "0"/g' config/Config-images.in
 #sed -i 's/ipad=${ipaddr:-"192.168.$((addr_offset++)).1"}/ipad=${ipaddr:-"10.0.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.248.0/g' package/base-files/files/bin/config_generate
 #sed -i 's/.hostname='OpenWrt'/.hostname='Sunvid'/g' package/base-files/files/bin/config_generate

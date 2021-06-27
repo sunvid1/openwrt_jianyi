e8467c6d4e6a6eec4dfe16ffbc4a14aa8f2e7728
SunvidWong <45497358+SunvidWong@users.noreply.github.com>
2021-06-27 21:09:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,15 +12,18 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.5.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/default "5"/default "0"/g' config/Config-images.in
-#sed -i 's/ipad=${ipaddr:-"192.168.$((addr_offset++)).1"}/ipad=${ipaddr:-"10.0.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
-#sed -i 's/255.255.255.0/255.255.248.0/g' package/base-files/files/bin/config_generate
-#sed -i 's/.hostname='OpenWrt'/.hostname='Sunvid'/g' package/base-files/files/bin/config_generate
-# echo "修改机器名称"  #默认名称OpenWrt
-#sed -i 's/OpenWrt/Sunvid/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/Sunvid/g' package/base-files/files/bin/config_generate
+# 修改启动0秒
+sed -i 's/default "5"/default "0"/g' config/Config-images.in
 
-# Modify the version number
-#sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# 修改掩码
+sed -i 's/255.255.255.0/255.255.248.0/g' package/base-files/files/bin/config_generate
+
+# 修改机器名称  #默认名称OpenWrt
+sed -i 's/set system.@system[-1].hostname='OpenWrt'/set system.@system[-1].hostname='Sunvid '/g' package/base-files/files/bin/config_generate
+
+# 修改固件编号
+#sed -i "DISTRIB_REVISION='R21.6.1'" >> /etc/openwrt_release
+
+# 修改固件尾巴名称
+sed -i "s/OpenWrt /Sunvid /g" package/lean/default-settings/files/zzz-default-settings

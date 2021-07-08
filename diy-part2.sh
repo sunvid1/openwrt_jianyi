b1def032e6a537cd4ba74e32c7e476239659173d
sunvid1 <86123106+sunvid1@users.noreply.github.com>
2021-07-09 00:57:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,19 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #修改默认IP
-sed -i ' s/192.168.1.1/10.0.5.1/g '包/基本文件/文件/bin/config_generate
+sed -i 's/192.168.1.1/10.0.5.1/g’ package/base-files/files/bin/config_generate
 
 # 修改主机名
 # sed -i 's/ OpenWrt/Sunvid /g' package/base-files/files/bin/config_generate
 
 # 修改启动0秒
-sed -i ' s/默认“5”/默认“0” /g '  config/Config-images.in
+sed -i 's/default "5"/default "0"/g' config/Config-images.in
 
 # 修改版本号
 # sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-设置
 
 # 修改掩码
-sed -i ' s/255.255.255.0/255.255.248.0/g '包/基本文件/文件/bin/config_generate
+sed -i 's/255.255.255.0/255.255.248.0/g' package/base-files/files/bin/config_generate
 
 # 修改机器名称 #默认名称OpenWrt
 sed -i ' s/set system.@system[-1].hostname= ' OpenWrt ' /set system.@system[-1].hostname= ' Sunvid ' /g ' package/base-files/files/bin/ config_generate
